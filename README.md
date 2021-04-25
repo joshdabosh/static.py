98558105a4271e4c74038f20380a63a634f675ff
JoshDaBosh <jwanggt@gmail.com>
2021-04-25 16:26:26 -0400

    feat: docker episode + modify docs

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
-# static.py
+# static.py: docker episode
 
-### What does it do?
+## What does it do?
 
-You give it a dynamically linked binary and it will make a directory that has all the dependencies (recursively). It also fixes the ld interpreter and libc paths so it will not segfault
+You give it a running Docker container and a dynamically linked binary inside that container.
 
-### How does it work?
+It will make a directory locally that has all the dependencies (recursively).  It also fixes the ld interpreter and libc paths so it will not segfault.
+
+## How does it work?
 
 It recursively calls ldd and copies all the required .so files. Then, it also needs to patch up the ld interpreter so it will not try to use the system ld rather than the one copied from the source host.
 
-### How do I use it
+## How do I use it
 
-For example, making a redistributable imagemagick:
+For example, making a redistributable `/usr/bin/ls` in running container `bbb`:
 
-`./lmao.py /usr/bin/convert`
+`./lmao.py bbb:/usr/bin/ls`
 
-### Should I use this in production?
+If you want to use this for simplifying binary exploitation the actual binary is the `<binary>.1` in `out/`. Remember to set `LD_LIBRARY_PATH=.` in your environment vars when running.
+
+## Should I use this in production?
 
 Yes and please email me so I can short your company stock. (Not financial advice btw)
--- lmao.py
+++ lmao.py
@@ -9,26 +9,53 @@ import shutil
 import subprocess
 
 regexp = re.compile(r'(.+ => )?(.+) \(0x[0-9a-f]{16}\)')
-ldd = shutil.which('ldd')
-assert ldd
 
-def visit(binary, outdir, visited, level=0):
+
+def run(cmd):
+    return subprocess.run(
+        cmd.split(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+
+def run_in_container(container, cmd):
+    return run(f'docker exec {container} {cmd}')
+
+
+def check_exists(container, binary):
+    output = run_in_container(container, f'test -f {binary}')
+    
+    if output.returncode != 0:
+        print(f"File not found: {container}:{binary}")
+        exit(1)
+
+
+def visit(container, binary, outdir, visited, level=0):
     if binary in visited:
         return
+    
     visited.add(binary)
 
     indent = '    '*level
     filename = os.path.basename(binary)
+    
+    src = container+":"+binary
     dst = os.path.join(outdir, filename)
-    assert not dst.startswith('/')
+    
+    assert not dst.startswith('/')    
+    
+    check_exists(container, binary)
+    
     if not os.path.exists(dst) or level == 0:
-        shutil.copyfile(binary, dst)
-        print (indent + '%s => %s' % (binary, dst))
+        run(f'docker cp -L {src} {dst}')
+        print (indent + f'{src} => {dst}')
+        
+    ldd_output = run_in_container(container, f'ldd {binary}')
 
-    libs = list(map(str.strip, subprocess.check_output([ldd, binary]).decode('ascii').split('\n')))
+    libs = list(map(str.strip, ldd_output.stdout.decode('ascii').strip().split('\n')))
+    
     for l in libs:
-        if not l: # skip empty line
-            continue
         if l == 'statically linked' or l.startswith('linux-vdso.so'):
             continue
         else:
@@ -36,37 +63,48 @@ def visit(binary, outdir, visited, level=0):
             assert m is not None
             libfile = m.group(2)
             assert libfile.startswith('/')
-            assert os.path.isfile(libfile)
-            visit(libfile, outdir, visited, level+1)
+            check_exists(container, libfile)
+            visit(container, libfile, outdir, visited, level+1)
 
     return dst
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print ('Usage: %s <binary>' % sys.argv[0])
+        print (f'Usage: {sys.argv[0]} <container>:<path to binary>')
+        exit(1)
+        
+    if len(sys.argv[1].split(":")) < 2:
+        print (f'Usage: {sys.argv[0]} <container>:<path to binary>')
         exit(1)
 
     patchelf = shutil.which('patchelf')
     if not patchelf:
-        print ('patchelf is not installed. Please install it')
+        print ('patchelf is not installed. Please install it.')
         exit(1)
-
-    my_meme = sys.argv[1]
+        
+    container, path = sys.argv[1].split(":")
+    
     outdir = 'out'
     if not os.path.exists(outdir):
         os.mkdir(outdir)
+    
+    res = run_in_container(container, 'which ldd')
+    
+    if res.returncode != 0:
+        print(res.stderr.decode('ascii').strip())
+        exit(1)
 
-    outfile = visit(my_meme, outdir, set())
+    outfile = visit(container, path, outdir, set())
 
     # Fix interpreter so it will use the correct ld and not segfault
-    print ('Patching %s interpreter and libc paths' % outfile)
+    print (f'Patching {outfile} interpreter and libc paths')
     ld_so = glob.glob(os.path.join(outdir, 'ld-linux-*.so*'))
-    print ('Found ld: %s' % ld_so)
+    print ('Found ld:', ld_so)
     assert len(ld_so) == 1
     ld_so = os.path.basename(ld_so[0])
     interpreter = './' + ld_so
-    print ('New interpreter path: %s' % interpreter)
+    print (f'New interpreter path: {interpreter}')
 
     subprocess.check_output([patchelf, '--set-interpreter', interpreter, '--set-rpath', '.', outfile])
 
@@ -74,11 +112,11 @@ if __name__ == '__main__':
     real_bin = outfile + '.1'
     shutil.move(outfile, real_bin)
     with open (outfile, 'w') as f:
-        f.write("#!/bin/sh\nLD_LIBRARY_PATH=. ./%s $*\n" % os.path.basename(real_bin))
+        f.write(f"#!/bin/sh\nLD_LIBRARY_PATH=. ./{os.path.basename(real_bin)} $*\n")
 
     # chmod everything +x lol
     for f in glob.glob(os.path.join(outdir, '*')):
         st = os.stat(f)
         os.chmod(f, st.st_mode | stat.S_IEXEC)
-
+    
     print ('Done')
